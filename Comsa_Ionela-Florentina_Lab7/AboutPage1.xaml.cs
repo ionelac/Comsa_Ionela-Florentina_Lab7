70f209320419f2ab468b7102bf861b434fa79ad1
ionelac <fcomsa40@gmail.com>
2022-11-30 19:19:31 +0200

    Add project files.

---
+++ Comsa_Ionela-Florentina_Lab7/AboutPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace Comsa_Ionela_Florentina_Lab7;
+
+public partial class AboutPage1 : ContentPage
+{
+	public AboutPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Comsa_Ionela-Florentina_Lab7/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Comsa_Ionela_Florentina_Lab7;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
